4a212608fdf5b6820c3c36a9d2496a391d476173
张泽洁 <3296592663@qq.com>
2025-05-19 16:49:18 +0800

    提交Node.js+Express
    提交图书馆完结项目，以及涉及到的全部知识点。以后更新均为后端项目学习理解

--- Express实战项目-本地图书馆/app.js
+++ Express实战项目-本地图书馆/app.js
@@ -43,7 +43,7 @@ module.exports = app;
 
 // 设置 Mongoose 连接
 const mongoose = require("mongoose");
-const mongoDB = "mongodb://127.0.0.1:27017/idiot";
+const mongoDB = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/idiot";
 
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
